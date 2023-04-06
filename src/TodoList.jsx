f05c1265bf46444f31ec7676e82b6c5531dd8ba9
Wesley Pereira <aventureiro-msn@hotmail.com>
2023-04-06 17:50:08 -0300

    Estrutura inicial do arquivo principal

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,7 +1,26 @@
-import React from "react";  
+import React from "react";
+import './TodoList.css';  
 
 function TodoList() {
-    return (<h1>Olá mundo</h1>)
+    return (
+        <div>
+            <h1>Lista de Tarefas</h1>
+            <form>
+                <input type="text" placeholder="Adicione uma tarefa"/>
+                <button type="submit">Add</button>
+            </form>
+            <div className="listaTarefas">
+                <div className="item">
+                    <span>Tarefa de exemplo</span>
+                    <button>Deletar</button>
+                </div>
+                <div className="item completo">
+                    <span>Tarefa de exemplo</span>
+                    <button>Deletar</button>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default TodoList;
